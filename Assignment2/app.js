d67a1b6d543d1d9696da2e06a71a77ff58849825
sonkar-amit <sonkar.amit08@gmail.com>
2016-12-09 21:21:09 +0530

    JS2 Assignment2

---
+++ Assignment2/app.js
@@ -0,0 +1,16 @@
+var num1 = 15;
+var num2 = 10;
+var num3 = 18;
+
+if(num1 > num2 && num1 > num3)
+{
+    console.log(num1);
+}
+else if( num2 > num1 && num2 > num3)
+{
+    console.log(num2);
+}
+else if(num3 > num1 && num3 > num2)
+{
+    console.log(num3);
+}
